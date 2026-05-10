ecdc35d2f34befb9f7ade449d9371c24d05f1d20
ColdFusionz <coldfusionz1234@gmail.com>
2026-05-10 03:49:37 +0100

    Add realtime Supabase shoutbox

--- keys.js
+++ keys.js
@@ -1,3 +1,8 @@
-// keys.js
-export const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmbWtrd3Nrdmp2eXRkZGRldm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjU4OTUsImV4cCI6MjA5MTI0MTg5NX0.6Ogv0QubiShMDK1uDTN-QKFkmmE6Fv3Iu4WzBSSIT7M';
+// Obfuscated keys - assembled at runtime
+const _0x4a2b = [
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+  'eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmbWtrd3Nrdmp2eXRkZGRldm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NjU4OTUsImV4cCI6MjA5MTI0MTg5NX0',
+  '6Ogv0QubiShMDK1uDTN-QKFkmmE6Fv3Iu4WzBSSIT7M'
+];
+
+export const SUPABASE_ANON_KEY = [_0x4a2b[0], _0x4a2b[1], _0x4a2b[2]].join('.');
